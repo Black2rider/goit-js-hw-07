d4da5b437f9bab7a224fbde51b02cb287a5aa246
Kostya Kusik <60221585+Black2rider@users.noreply.github.com>
2023-03-30 21:59:02 +0300

    commit task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,8 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-{/* <li class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</li> */}
-
 const gallery = document.querySelector(".gallery");
+
 const markup = galleryItems.map(
    ({preview, original, description}) =>
    `<li class="gallery__item">
@@ -28,7 +18,28 @@ const markup = galleryItems.map(
 );
 
 gallery.insertAdjacentHTML("beforeend", markup.join(""));
+gallery.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  const {target} = evt;
+  if (!target.classList.contains("gallery__image")) {
+    return;
+  }
+  if (target.nodeName === "IMG") {
+  const instance = basicLightbox.create
+  (`<img src="${target.dataset.source}" width="800" 
+  height="600">`, {
+    onClose: () => {
+      document.addEventListener("keydown", Keys)}});
+      function Keys(evt) {
+        if (evt.key === "Escape") {
+            instance.close();
+        }
+      }
+  instance.show();  
+  }
+}
 
 
-
-//console.log(galleryItems);
+//console.log(galleryItems)

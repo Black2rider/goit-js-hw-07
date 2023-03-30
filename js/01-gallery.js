c6408d365961658344f65ee180ff5c5b4863befa
Kostya Kusik <60221585+Black2rider@users.noreply.github.com>
2023-03-30 17:34:29 +0300

    gallery 1 reduct

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+{/* <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</li> */}
+
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems.map(
+   ({preview, original, description}) =>
+   `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+     <img
+       class="gallery__image"
+       src="${preview}"
+       data-source="${original}"
+       alt="${description}"
+     />
+   </a>
+ </li>`
+);
+
+gallery.insertAdjacentHTML("beforeend", markup.join(""));
+
+
+
+//console.log(galleryItems);

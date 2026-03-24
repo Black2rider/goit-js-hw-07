84e0cee7a2edf342b1c9454d1ac315e0c1862dab
Kostya_Kusik <kostya_kysik@ukr.net>
2026-03-24 22:51:14 +0200

    3

--- js/task-3.js
+++ js/task-3.js
@@ -14,5 +14,9 @@ const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
 input.addEventListener('input', event => {
-  output.textContent = event.currentTarget.value;
+  if (event.currentTarget.value.trim() === '') {
+    output.textContent = 'Anonymous';
+  } else {
+    output.textContent = event.currentTarget.value.trim();
+  }
 });
